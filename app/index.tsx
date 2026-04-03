5598babeb4b2439151afde5730b57fb745d6b046
ishapanchal07 <isha7112003@gmail.com>
2026-04-03 17:23:28 +0530

    dependencys added

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import {
+  Text,
+  View
+} from "react-native";
+
+type Song = {
+  name: string;
+  url: string;
+};
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
